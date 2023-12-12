64b6e3dbef9faf3ef76a6b5219eba1407de1d399
shahin <shahin.aboulhassanibavili2021@campus-eni.fr>
2023-12-12 18:45:59 +0100

    HashSet

--- src/test/java/collection/set/SetTest.java
+++ src/test/java/collection/set/SetTest.java
@@ -6,10 +6,11 @@ import org.junit.jupiter.api.Test;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class SetTest {
     @Test
-    void should_Remove_Duplication() {
+    void hashSet_Should_Remove_Duplication() {
         Set<String> animals = new HashSet<>();
         animals.add("Monkey");
         animals.add("Zebra");
@@ -34,10 +35,26 @@ public class SetTest {
     }
 
     @Test
-    void should_Maintain_Insertion_Order() {
+    void linkedHashSet_Should_Maintain_Insertion_Order() {
         Set<String> animals = new LinkedHashSet<>();
         animals.add("Monkey");
         animals.add("Zebra");
+        animals.add("Elephant");
+        animals.add("Crocodile");
+        animals.add("Lion");
+
+        for (String animal : animals) {
+            System.out.println(animal);
+        }
+    }
+
+    @Test
+    void treeSet_Should_Sort_Data() {
+        Set<String> animals = new TreeSet<>();
+        animals.add("Monkey");
+        animals.add("Zebra");
+        animals.add("Elephant");
+        animals.add("Elephant");
         animals.add("Elephant");
         animals.add("Crocodile");
         animals.add("Lion");
