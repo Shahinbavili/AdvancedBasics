5e1623b471ed967f508cfa8898d7f2bb4e2a35bd
shahin <shahin.aboulhassanibavili2021@campus-eni.fr>
2023-12-12 18:46:14 +0100

    HashMap

---
+++ src/test/java/collection/map/MapTest.java
@@ -0,0 +1,46 @@
+package collection.map;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class MapTest {
+    @Test
+    void hashMap_Should_Have_Unique_Keys_And_Can_Duplicated_Values() {
+        // Creating a Hashtable to store geometrical shapes and their colors
+        Map<String, String> geometrics = new HashMap<>();
+        geometrics.put("Triangle", "Red");
+        geometrics.put("Rectangle", "Blue");
+        geometrics.put("Circle", "Yellow");
+        geometrics.put("Square", "Red");
+
+        Set<Map.Entry<String, String>> entries = geometrics.entrySet();
+        // Iterating over the entries and printing key-value pairs
+        for (Map.Entry<String, String> entry : entries) {
+            String key = entry.getKey();
+            String value = entry.getValue();
+            System.out.println("The key is " + key + " And its value is " + value + ".");
+        }
+    }
+
+    @Test
+    void linkedHashMap_Should_Sort_Insertion_Order() {
+        // Creating a Hashtable to store geometrical shapes and their colors
+        Map<String, String> geometrics = new LinkedHashMap<>();
+        geometrics.put("Triangle", "Red");
+        geometrics.put("Rectangle", "Blue");
+        geometrics.put("Circle", "Yellow");
+        geometrics.put("Square", "Red");
+
+        Set<Map.Entry<String, String>> entries = geometrics.entrySet();
+        // Iterating over the entries and printing key-value pairs
+        for (Map.Entry<String, String> entry : entries) {
+            String key = entry.getKey();
+            String value = entry.getValue();
+            System.out.println("The key is " + key + " And its value is " + value + ".");
+        }
+    }
+}
