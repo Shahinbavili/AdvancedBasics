f43ce0ced98082c5254c0876dcacaa11dde806f1
shahin <shahin.aboulhassanibavili2021@campus-eni.fr>
2023-12-14 12:17:54 +0100

    try catch

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -5,8 +5,15 @@ import static java.lang.Double.parseDouble;
 public class Calculator {
 
     public String add(String adadAval, String adadDovom) {
-        double d1 = parseDouble(adadAval);
-        double d2 = parseDouble(adadDovom);
+
+        double d1, d2;
+        try {
+            d1 = parseDouble(adadAval);
+            d2 = parseDouble(adadDovom);
+        } catch (NumberFormatException nfe) {
+            System.out.println("The entered number format is not allowed");
+            throw nfe;
+        }
         double result = d1 + d2;
         return String.valueOf(result);
     }
