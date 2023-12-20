acd4051b4d6aee59ace899c76c9ef20b6db9f56b
shahin <shahin.aboulhassanibavili2021@campus-eni.fr>
2023-12-20 09:54:10 +0100

    File In Put Stream

--- src/test/java/io/FileWriterTest.java
+++ src/test/java/io/FileWriterTest.java
@@ -1,23 +1,39 @@
 package io;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
+import static java.lang.System.out;
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class FileWriterTest {
+
+    private static final String FILE_NAME = "test.txt";
+    private static final StringBuilder SENTENCE = new StringBuilder();
+
     @Test
     void should_Write_In_A_File() {
-        try (FileOutputStream fileOutputStream = new FileOutputStream("test.txt")) {
+        try (FileOutputStream fileOutputStream = new FileOutputStream(FILE_NAME);
+             FileInputStream fileInputStream = new FileInputStream(FILE_NAME)) {
             fileOutputStream.write("Hello World!".getBytes(UTF_8));
+
+            int read;
+            while ((read = fileInputStream.read()) != -1) {
+                SENTENCE.append((char) read);
+            }
+            out.println(SENTENCE);
+            Assertions.assertThat(SENTENCE.toString()).isEqualTo("Hello World!");
         } catch (FileNotFoundException e) {
-            System.out.println("The file test.txt dose not exist!");
+            out.println("The file test.txt dose not exist!");
             throw new RuntimeException(e);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+
     }
 }
